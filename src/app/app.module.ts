89c5635b306b2b9de9c30f6fa34a58b982e26b9d
Maxim Wilman <mwilman@brockhaus-ag.de>
2019-07-26 16:40:31 +0200

    part2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
-import { AppComponent } from './app.component';
-import { ProductPageComponent } from './product-page/product-page.component';
+import {AppComponent} from './app.component';
+import {ProductPageComponent} from './product-page/product-page.component';
+import {ProductDescriptionComponent} from './product-description/product-description.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductPageComponent
+    ProductPageComponent,
+    ProductDescriptionComponent
   ],
   imports: [
     BrowserModule,
